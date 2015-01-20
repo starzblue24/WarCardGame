74b02039548617ad4d20e3ddcde1b810ed2ac715
Naimisha Badri <starzblue24@gmail.com>
2015-01-19 19:46:14 -0700

    Update CardList.java

--- src/CardList.java
+++ src/CardList.java
@@ -1,1 +1,36 @@
 // create class CardList
+import java.util.*;
+public class CardList
+{
+  private ArrayList<Card> deck;
+  private Random random=new Random();
+  public CardList()
+  {
+    deck=new ArrayList<Card>();
+  }
+  public int getSize()
+  {
+    return deck.size();
+  }
+  public void addCardToBottom(Card c)
+  {
+    deck.add(c);
+  }
+  public void addCardToTop(Card c)
+  {
+    deck.add(0,c);
+  }
+  public Card takeCardFromTop()
+  {
+    return deck.remove(0);
+  }
+  public Card removeRandomCard()
+  {
+      return deck.remove(random.nextInt(deck.size()));
+  }
+  public Card getDeck(int y)
+  {
+    return deck.get(y);
+}
+
+}
